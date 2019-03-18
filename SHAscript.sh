ffa29d0a76ae69ab90273914186cf30868658949
Mitchell P. Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2019-03-17 17:43:18 -0700

    Here's the MVP

---
+++ SHAscript.sh
@@ -0,0 +1,11 @@
+# Isthmus 2019/02
+# not recommmended for use
+echo Type message string, press [enter]
+read MESSAGE
+echo
+$MESSAGE|sha256sum
+echo "chars:   ^10       ^20       ^30       ^40       ^50       ^60"
+echo
+echo Press [enter] to clear the screen
+read WAIT
+clear
